fde4abd79872c0a8f7aa1915d96f5e84f6e41a02
unknown <jayajn98@gmail.com>
2022-11-13 03:27:04 +0530

    db config update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,6 +2,6 @@ const MONGOHOST = 'containers-us-west-93.railway.app',
 MONGOPASSWORD = 'hSvR5WSKO66MbucGDegN',
 MONGOPORT = 6760,
 MONGOUSER = 'mongo',
-MONGO_URL = `mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}`
+MONGO_URL = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`
 
 module.exports = MONGO_URL;
