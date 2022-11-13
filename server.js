2880cde999d87180e71ac4313645dabe42b422a1
unknown <jayajn98@gmail.com>
2022-11-13 21:14:24 +0530

    cors

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const mongoose = require('mongoose');
 const MONGO_URL = require('./config/dbConfig');
 const walletRoutes = require('./routes/wallet');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || '3000';
 
 require('./prod')(app);
-app.use(express.static(__dirname + '/client'));
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 app.use('/api', walletRoutes)
 app.listen(port, () => console.log(`Server listening at ${port}`))
 mongoose.connect(`${MONGO_URL}`).then(() => {
